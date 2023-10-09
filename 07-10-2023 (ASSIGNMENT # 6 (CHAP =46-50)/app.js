e1503c4a04f8c332a8041ab6822770153e0636ef
SabihBaig <143556056+SabihBaig@users.noreply.github.com>
2023-10-09 15:23:01 +0500

    Add files via upload

---
+++ 07-10-2023 (ASSIGNMENT # 6 (CHAP =46-50)/app.js
@@ -0,0 +1,57 @@
+// // Question # 01
+
+// function greet(){
+//     alert('Hey JS developer')
+// }    
+
+// Question # 04
+// const scrolltotoplink = document.getElementById('scrollToToplink')
+// scrolltotoplink.addEventListener('click', function(event) {
+//     event.preventDefault();
+// window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+// });
+// })
+
+// // Question # 05 
+// function playAudio(){
+//     X.playAudio(); 
+// }
+
+// Question # 07
+// document.getElementById("myBtn").addEventListener("click", function() {
+//     alert("Hooray!");
+//   });
+
+// Question # 09
+// function playAudio(){
+//         X.playAudio(); 
+//     }
+
+// CHAPTER # 49,50
+
+// Question # 01
+
+// var user = +prompt("Enter Your Value")
+// console.log(user)
+
+
+// Question # 02
+// function getValue() {
+    
+//     var checkboxes =
+//         document.getElementsByName('laptop');
+//     var result = "";
+//     for (var i = 0; i < checkboxes.length; i++) {
+//         if (checkboxes[i].checked) {
+//             result += checkboxes[i].value
+//                 + " " + " Laptop, ";
+//         }
+//     }
+//     console.log("You have selected Mobile Phone")}
+
+// Question # 03
+// function myFunction() {
+//     console.log("banana")
+//   }
